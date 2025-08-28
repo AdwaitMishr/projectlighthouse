e143595731bd4128912918024a0326f2091eca3d
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-08-28 20:08:25 +0530

    initial: turborepo created with express server and nextjs web

---
+++ apps/server/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+
+const port = 8080;
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+
+
+server.listen(port,()=>{
+    console.log(`Project Lighthouse Express server operational.`);
+})
